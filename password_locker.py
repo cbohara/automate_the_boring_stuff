6cb0ae7b5dde158c256b4a196536774a81ca6507
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-01-02 19:41:39 -0800

    will not be posting password manager program

--- password_locker.py
+++
@@ -1,6 +0,0 @@
-""" It is best practice to have a different password for every account. This
-simple password management program will make it easy to have a variety of passwords.
-You want to be able to run this program with a command line argument that is the
-account’s name. That account’s password will be copied to the clipboard so that
-the user can paste it into a Password field. This way, the user can have long,
-complicated passwords without having to memorize them."""
