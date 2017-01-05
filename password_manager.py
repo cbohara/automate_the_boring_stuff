08f311bda9a2bcba7be37f90f9abd0cf4206764a
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-01-04 18:12:03 -0800

    building password manager

---
+++ password_manager.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import sys
+import pyperclip
+
+PASSWORDS = {}
+
+def add_account(account_name, user_name, password):
+    """Add account to PASSWORD dictionary and generate random password."""
+
+def main(script):
+    try:
+        account = sys.argv[1]
+    except IndexError:
+        print('specify account name - python pw.py [account]')
+    else:
+        if account or account.title() in PASSWORDS:
+            pyperclip.copy(PASSWORDS[account][1])
+            print('Password for ' + account + ' copied to clipboard.')
+        else:
+            print('There is no account named ' + account)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
