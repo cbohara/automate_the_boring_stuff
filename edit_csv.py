adaccb2767eeebc376e4c95f0e1e23d0db0f20cf
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-01-06 21:46:37 -0800

    refactored code to make it more DRY

--- edit_csv.py
+++ edit_csv.py
@@ -1,9 +1,16 @@
-"""Script for updating fields in CSV file."""
 import sys
 import csv
 
 
+def partial_url(row, index):
+    """Determine if value is only a partial url and needs to be a full url."""
+    if len(row[index]) != 0:
+        if row[index][0] == '/':
+            return True
+    return False
+
 def prepend_url(string):
+    """Return full url."""
     s_to_l = list(string)
     s_to_l.insert(0, 'https://www.nutanix.com')
     full_url = ''.join(s_to_l)
@@ -11,31 +18,26 @@ def prepend_url(string):
 
 
 def main(script):
-    # read in csv file
-    original_file = open('data/customers_edited.csv')
+    """Script for updating url fields in CSV file."""
+    original_file = open('data/customers_original.csv')
     original_object = csv.reader(original_file)
 
-    # edit content and append to output list
     output_csv = []
     for row in original_object:
-        if len(row[4]) != 0:
-            if row[4][0] == '/':
-                full_url = prepend_url(row[4])
-                row[4] = full_url
+        if partial_url(row, 4):
+            full_url = prepend_url(row[4])
+            row[4] = full_url
 
-        if len(row[5]) != 0:
-            if row[5][0] == '/':
-                full_url = prepend_url(row[5])
-                row[5] = full_url
+        if partial_url(row, 5):
+            full_url = prepend_url(row[5])
+            row[5] = full_url
 
-        if len(row[6]) != 0:
-            if row[6][0] == '/':
-                full_url = prepend_url(row[6])
-                row[6] = full_url
+        if partial_url(row, 6):
+            full_url = prepend_url(row[6])
+            row[6] = full_url
 
         output_csv.append(row)
 
-    # write edits to new file
     with open('data/customers_edited.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
 
