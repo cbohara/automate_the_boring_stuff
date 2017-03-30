acdb7634930cba7d0af22d5bec6b05e5a71523e0
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-03-29 19:05:46 -0700

    writing filtered matrix to csv file

--- memory_per_node.py
+++ memory_per_node.py
@@ -3,20 +3,21 @@ import csv
 
 
 def node_per_timestamp(matrix, timestamp):
-    """Filter out input matrix so only one row for each node is returned."""
+    """Filter out input matrix so only row with timestamp in each node is returned."""
+    timestamp_matrix = []
     for row in matrix:
-        if timestamp in row[2]:
-            print(timestamp)
-
+        if timestamp in row[1]:
+            timestamp_matrix.append(row)
+    return timestamp_matrix
 
 def main(script):
-    """Find unique rows in csv file."""
+    """Create csv file containing node memory info for specific time."""
     try:
         # ensure user entered csv file and timestamp
         csv_file = sys.argv[1]
         timestamp = sys.argv[2]
     except IndexError:
-        print('python3 unique_rows.py [csv_file] [timestamp]')
+        print('python3 memory_per_node.py [csv_file] [timestamp]')
     else:
         # read in csv file
         with open(csv_file, 'r') as file_input:
@@ -24,7 +25,13 @@ def main(script):
             # create matrix from csv file
             matrix = [line for line in file_reader]
 
-    node_per_timestamp(matrix, timestamp)
+        # create matrix only containing info for specific timestamp
+        timestamp_matrix = node_per_timestamp(matrix, timestamp)
+
+        # write to csv file
+        with open('data/'+timestamp, 'w') as file_output:
+            file_writer = csv.writer(file_output, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
+            file_writer.writerows(timestamp_matrix)
 
 
 if __name__ == "__main__":
