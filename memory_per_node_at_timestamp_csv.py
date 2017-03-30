115a1aabc22dad77c72ab010766633ce9e24e4ce
Charlie O'Hara <cohara@algebraixdata.com>
2017-03-30 10:03:55 -0700

    added conversion function so it is easier to read memory data

--- memory_per_node_at_timestamp_csv.py
+++ memory_per_node_at_timestamp_csv.py
@@ -2,11 +2,18 @@ import sys
 import csv
 
 
-def node_at_timestamp(matrix, timestamp):
+def convert_KiB_to_GiB(memory):
+    """Convert KiB to GiB and round to two decimal places."""
+    return round((memory / 1048576), 2)
+
+
+def nodes_at_timestamp(matrix, timestamp):
     """Return matrix containing node info at timestamp."""
-    timestamp_matrix = [timestamp]
+    timestamp_matrix = []
     for row in matrix:
         if timestamp in row[1]:
+            if row[2] != 'null':
+                row[2] = convert_KiB_to_GiB(float(row[2]))
             timestamp_matrix.append(row)
     return timestamp_matrix
 
@@ -20,17 +27,19 @@ def main(script):
     except IndexError:
         print('python3 memory_per_node.py [csv_file] [timestamp]')
     else:
-        # read in csv file
+        # read in grafana csv file
         with open(csv_file, 'r') as file_input:
-            file_reader = csv.reader(file_input)
+            file_reader = csv.reader(file_input, delimiter=';')
             # create matrix from csv file
             matrix = [line for line in file_reader]
 
         # create matrix only containing info for specific timestamp
-        timestamp_matrix = node_at_timestamp(matrix, timestamp)
+        timestamp_matrix = nodes_at_timestamp(matrix, timestamp)
+
         # write to csv file
-        with open('data/'+timestamp, 'w') as file_output:
+        with open('data/memory_per_node_at_'+timestamp+'.csv', 'w') as file_output:
             file_writer = csv.writer(file_output, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
+            file_writer.writerow(['Node', 'Time','Memory(GiB)'])
             file_writer.writerows(timestamp_matrix)
 
 
