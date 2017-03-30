5b569001de3be5d2673fbc963ba6e885da2d37fe
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-03-30 07:41:25 -0700

    attempting to revert back

---
+++ working_memory_csv.py
@@ -0,0 +1,38 @@
+import sys
+import csv
+
+
+def node_at_timestamp(matrix, timestamp):
+    """Return matrix containing node info at timestamp."""
+    timestamp_matrix = [timestamp]
+    for row in matrix:
+        if timestamp in row[1]:
+            timestamp_matrix.append(row)
+    return timestamp_matrix
+
+
+def main(script):
+    """Create csv file containing node memory info for specific time."""
+    try:
+        # ensure user entered csv file and timestamp
+        csv_file = sys.argv[1]
+        timestamp = sys.argv[2]
+    except IndexError:
+        print('python3 memory_per_node.py [csv_file] [timestamp]')
+    else:
+        # read in csv file
+        with open(csv_file, 'r') as file_input:
+            file_reader = csv.reader(file_input)
+            # create matrix from csv file
+            matrix = [line for line in file_reader]
+
+        # create matrix only containing info for specific timestamp
+        timestamp_matrix = node_at_timestamp(matrix, timestamp)
+        # write to csv file
+        with open('data/'+timestamp, 'w') as file_output:
+            file_writer = csv.writer(file_output, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
+            file_writer.writerows(timestamp_matrix)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
