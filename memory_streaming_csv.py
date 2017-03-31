c81d7a52aab0b6112deb4fb67fb0b7826dfded86
Charlie O'Hara <cohara@algebraixdata.com>
2017-03-31 15:48:53 -0700

    mvp

--- memory_streaming_csv.py
+++ memory_streaming_csv.py
@@ -4,26 +4,9 @@ from datetime import datetime as dt
 from datetime import timedelta
 
 
-def time_series(start_date, start_time, end_date, end_time, interval):
-    """Create list of times to be found in grafana csv."""
-    # convert input time strings into datetime object
-    start_time = dt.strptime(start_date + start_time, '%Y-%m-%d%H:%M:%S')
-    end_time = dt.strptime(end_date + end_time, '%Y-%m-%d%H:%M:%S')
-    # store time series
-    times = []
-    # iterate from start time to end time and append to times list 
-    current = start_time
-    while current <= end_time:
-        times.append(current)
-        current += timedelta(seconds=int(interval))
-    # return times as required to search within grafana csv
-    time_series = []
-    # change time format into string that will be serched in grafana csv
-    for time in times:
-        back_to_string = dt.strftime(time, '%Y-%m-%d%H:%M:%S')
-        grafana_string = back_to_string[:10] + "T" + back_to_string[10:] + ".000Z"
-        time_series.append(grafana_string)
-    return time_series
+def convert_KiB_to_GiB(memory):
+    """Convert KiB to GiB and round to two decimal places."""
+    return round((float(memory) / 1048576), 2)
 
 
 def node_names(matrix):
@@ -36,9 +19,50 @@ def node_names(matrix):
     return nodes
 
 
-def convert_KiB_to_GiB(memory):
-    """Convert KiB to GiB and round to two decimal places."""
-    return round((memory / 1048576), 2)
+def increment_datetime(start_date, start_time, end_date, end_time, interval):
+    """Use datetime module to increment time and append to times list."""
+    # convert input time strings into datetime object
+    start_time = dt.strptime(start_date + start_time, '%Y-%m-%d%H:%M:%S')
+    end_time = dt.strptime(end_date + end_time, '%Y-%m-%d%H:%M:%S')
+    # store time series
+    times = []
+    # iterate from start time to end time and append to times list 
+    current = start_time
+    while current <= end_time:
+        times.append(current)
+        current += timedelta(seconds=int(interval))
+    return times
+
+
+def header_times(start_date, start_time, end_date, end_time, window):
+    """Format header times to clarify the time window of each memory value."""
+    # list of timestamps that need to be included in header 
+    times = increment_datetime(start_date, start_time, end_date, end_time, window)
+    # change time format to show range of window
+    formatted_times = [dt.strftime(time, '%H:%M:%S') for time in times]
+    # specify time range of window
+    header_times = []
+    for index, value in enumerate(formatted_times):
+        if index == 0:
+            pass
+        else:
+            time_range = formatted_times[index-1] + "-" + formatted_times[index]
+            header_times.append(time_range)
+    return header_times
+
+
+def grafana_times(start_date, start_time, end_date, end_time, frequency):
+    """Create list of times to be found in grafana csv."""
+    # list of timestamps that need to be found in grafana csv
+    times = increment_datetime(start_date, start_time, end_date, end_time, frequency)
+    # format times as required to search within grafana csv
+    grafana_times = []
+    # change time format into string that will be serched in grafana csv
+    for time in times:
+        dt_to_string = dt.strftime(time, '%Y-%m-%d%H:%M:%S')
+        grafana_string = dt_to_string[:10] + "T" + dt_to_string[10:] + ".000Z"
+        grafana_times.append(grafana_string)
+    return grafana_times
 
 
 def filter_for_times(matrix, times):
@@ -49,20 +73,39 @@ def filter_for_times(matrix, times):
         for row in matrix:
             if str(time) in str(row[1]):
                 if row[2] != 'null':
-                    row[2] = convert_KiB_to_GiB(float(row[2]))
+                    row[2] = convert_KiB_to_GiB(row[2])
                 output_matrix.append(row)
     return output_matrix
 
 
-def filter_by_node(matrix, nodes):
+def max_per_window(times, frequency, window):
+    """Return aggregated list of max values for given time windows."""
+    max_per_window = []
+    # add node name to beginning of the list
+    max_per_window.append(times[0])
+    # calculate number of values per window
+    num_values = int(window) // int(frequency)
+    # start and end of window interval
+    start = 1
+    end = 1 + num_values
+    # traverse list and find the max value in the window and add to list to output
+    while end <= len(times):
+        max_per_window.append(max(times[start:end]))
+        start = end
+        end = start + num_values
+    return max_per_window
+
+
+def filter_by_node(matrix, nodes, frequency, window):
     """Return matrix that will be converted into the final csv file."""
     final_matrix = []
     for node in nodes:
-        node_array =[node]
+        node_array = [node]
         for row in matrix:
             if node == row[0]:
                 node_array.append(row[2])
-        final_matrix.append(node_array)
+        aggregate_list = max_per_window(node_array, frequency, window)
+        final_matrix.append(aggregate_list)
     return final_matrix
 
 
@@ -75,9 +118,10 @@ def main(script):
         start_time = sys.argv[3]
         end_date = sys.argv[4]
         end_time = sys.argv[5]
-        interval = sys.argv[6]
+        frequency = sys.argv[6]
+        window = sys.argv[7]
     except IndexError:
-        print('python3 memory_streaming_csv.py [csv_file] [start_date] [start_time] [end_date] [end_time] [analysis window(seconds)]')
+        print('python3 memory_streaming_csv.py [csv_file] [start_date] [start_time] [end_date] [end_time] [data collection frequency(sec)] [window interval(sec)]')
     else:
         # read in grafana csv file
         with open(csv_file, 'r') as file_input:
@@ -86,17 +130,18 @@ def main(script):
             matrix = [line for line in file_reader]
 
         # list of times that need to be found in grafana csv 
-        times = time_series(start_date, start_time, end_date, end_time, interval)
+        times = grafana_times(start_date, start_time, end_date, end_time, frequency)
         # list of nodes 
         nodes = node_names(matrix)
         # find node data for times in time_series
         filtered_matrix = filter_for_times(matrix, times)
         # need to get filtered matrix into desired output format
-        final_output = filter_by_node(filtered_matrix, nodes)
+        final_output = filter_by_node(filtered_matrix, nodes, frequency, window)
         # write to csv file
         with open('data/'+start_date+"_"+start_time+'.csv', 'w') as file_output:
             file_writer = csv.writer(file_output, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
-            file_writer.writerow(['Node name'] + [time[11:19] for time in times])
+            file_writer.writerow(['Node name', 'Maximum memory value (GiB) in time window'])
+            file_writer.writerow([''] + header_times(start_date, start_time, end_date, end_time, window))
             file_writer.writerows(final_output)
 
 
