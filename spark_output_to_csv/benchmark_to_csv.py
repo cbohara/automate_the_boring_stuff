02b519a2d6717eee3e78b402d5199705ddbe839f
cohara <cohara@algebraixdata.com>
2017-04-26 11:34:50 -0700

    refactor to specify level and data size

--- spark_output_to_csv/benchmark_to_csv.py
+++ spark_output_to_csv/benchmark_to_csv.py
@@ -3833,15 +3833,20 @@ url += "[" + ",".join(str(run) for run in runtimes) + "]"
 print("""-------- Query Finished. --------
 -------- Benchmark Results Viewer: {} --------""".format(url))
 
-benchmark_version = "L1_1M"
+bench_or_base = "benchmark"
+level = "L1"
+data_size = "1M"
 
-if not os.path.exists("/home/cohara/github/automate/data/" + benchmark_version + ".csv"):
-    with open("/home/cohara/github/automate/data/" + benchmark_version + ".csv", "w") as csv_file:
+if not os.path.exists("./results/"+bench_or_base+"/"):
+    os.makedirs("./results/"+bench_or_base+"/")
+
+if not os.path.exists("./results/"+bench_or_base+"/"+level+"_"+data_size+".csv"):
+    with open("./results/"+bench_or_base+"/"+level+"_"+data_size+".csv", "w") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Timestamp"] + list(range(1, len(runtimes)+1)))
         csv_file.close()
 
-with open("/home/cohara/github/automate/data/" + benchmark_version + ".csv", "a") as csv_file:
+with open("./results/"+bench_or_base+"/"+level+"_"+data_size+".csv", "a") as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow([datetime.now()] + runtimes)
     csv_file.close()
