6596225fe47dc796373ef0cb3258ca05856a2796
cohara <cohara@algebraixdata.com>
2017-05-10 15:42:12 -0700

    getting a handle on logging module

--- debugging/factorial_log.py
+++ debugging/factorial_log.py
@@ -1,25 +1,15 @@
-import sys
 import logging
-
+logging.basicConfig(filename="logger.txt", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of program')
 
 def factorial(n):
-    """Return factorial value of n input."""
-    logging.debug("Start of factorial(%s)" % (n))
+    logging.debug('Start of factorial(%s%%)' % (n))
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
-        logging.debug("i is " + str(i) + ", total is " + str(total))
-    logging.debug("End of factorial(%s)" % (n))
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
     return total
+    logging.debug('End of factorial(%s%%)' % (n))
 
-def main(script):
-    """Basic example of using logging module."""
-    logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(messages)s')
-    logging.debug("Start of program")
-
-    print(factorial(5))
-    logging.debug("End of program")
-
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+print(factorial(5))
+logging.debug('End of program')
