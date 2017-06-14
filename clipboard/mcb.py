6a2e1cd8e38c0e6afcc990536a2642c980327395
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-14 07:54:39 -0700

    added additional functionality

--- clipboard/mcb.py
+++ clipboard/mcb.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import sys
+import os
 import shelve
 import pyperclip
 
@@ -13,14 +14,25 @@ def main(args):
         print("python3 mcb.py [command] [specify key or all]")
     else:
         with shelve.open('multiclipboard') as mcb_shelf:
-            if command == 'save':
-                mcb_shelf[key] = pyperclip.paste()
+            # execute command for specific key
+            if key != 'all':
+                if command == 'save':
+                    mcb_shelf[key] = pyperclip.paste()
 
-            if command == 'load':
-                pyperclip.copy(mcb_shelf[key])
+                if command == 'load':
+                    pyperclip.copy(mcb_shelf[key])
 
-            if command == 'list':
-                pyperclip.copy(str(list(mcb_shelf.items())))
+                if command == 'delete':
+                    del mcb_shelf[key]
+
+            # execute command for all keys
+            if key == 'all':
+                if command == 'list':
+                    pyperclip.copy(str(list(mcb_shelf.items())))
+
+                if command == 'delete':
+                    os.remove('multiclipboard')
+
 
 
 if __name__ == "__main__":
