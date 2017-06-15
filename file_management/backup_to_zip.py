43c9c5f4bfcfeb9e8ac4d7a494d13dc46f724019
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-15 08:24:11 -0700

    fixed code in order to avoid generating absolute folder path when unzipping

--- file_management/backup_to_zip.py
+++ file_management/backup_to_zip.py
@@ -11,7 +11,7 @@ def main(args):
     except IndexError:
         print("python3 backup_to_zip.py [folder in cwd]")
     else:
-        folder = os.path.abspath(folder)
+        folder = os.path.relpath(folder)
 
         # determine the next increment value to attach to filename
         n = 1
@@ -23,6 +23,18 @@ def main(args):
                 break
             # otherwise increment n and check again
             n += 1
+
+        # create zip file
+        print("Creating {}".format(zipfile_name))
+        with zipfile.ZipFile(zipfile_name, "w") as backup:
+            # walk the folder tree and compress the files in each folder
+            for foldername, subfolders, filenames in os.walk(folder):
+                print("Adding files in {}".format(foldername))
+                backup.write(foldername)
+                for filename in filenames:
+                    print("Adding {}".format(os.path.join(foldername, filename)))
+                    backup.write(os.path.join(foldername, filename))
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
