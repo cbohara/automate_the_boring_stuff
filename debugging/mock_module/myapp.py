ecaec6c48daac228878ad6a52ef040dccd402569
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-15 16:07:54 -0700

    using logging within a module

---
+++ debugging/mock_module/myapp.py
@@ -0,0 +1,14 @@
+import logging
+import mylib
+
+
+def main():
+    logging.basicConfig(filename='myapp.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO)
+    logging.info('Started')
+    mylib.do_something()
+    logging.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
