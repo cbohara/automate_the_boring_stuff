3d281365c355720ca6dc86db971bd24a9873f2d7
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-14 17:04:31 -0700

    renaming files from MM-DD-YYYY to DD-MM-YYYY

--- file_management/rename_date.py
+++ file_management/rename_date.py
@@ -5,7 +5,7 @@ import shutil
 
 
 def main():
-    """Rename files from American MM-DD-YYYY to European DD-MM-YYYY date format."""
+    """Rename files from American MM-DD-YYYY to international DD-MM-YYYY date format."""
     # create regex that will match American MM-DD-YYYY format
     american_date_regex = re.compile(r"""
         ^(.*?)              # all text before the date
@@ -16,16 +16,34 @@ def main():
     """, re.VERBOSE)
 
     # loop over files in the current working directory
+    for filename in os.listdir("."):
+        match = american_date_regex.search(filename)
 
-    # skip files without a date
+        # if the file doesn't contain a date continue through for loop 
+        if match == None:
+            continue
 
-    # get the different parts of the filename
+        # get the different parts of the filename
+        before = match.group(1)
+        month = match.group(2)
+        day = match.group(4)
+        year = match.group(6)
+        after = match.group(8)
 
-    # form the European DD-MM-YYYY date format
+        # form the international DD-MM-YYYY date format 
+        intl_filename = before + day + "-" + month + "-" + year + after
 
-    # get full absolute file paths
+        # get full absolute file paths
+        abs_dir = os.path.abspath(".")
+        filename = os.path.join(abs_dir, filename)
+        intl_filename = os.path.join(abs_dir, intl_filename)
 
-    # rename files
+        # rename the file
+        print("Renaming {} to {}".format(filename, intl_filename))
+#        shutil.move(filename, intl_filename)
+
+
+        # rename files
 
 
 if __name__ == "__main__":
