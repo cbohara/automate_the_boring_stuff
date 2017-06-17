aae76dbc92237de26025f282b01fc3f8c4c93d03
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-16 17:49:11 -0700

    minor refactor

--- timer/pomodoro.py
+++ timer/pomodoro.py
@@ -7,20 +7,18 @@ import time
 def main(args):
     """Build simple pomodoro timer."""
     try:
-        iterations = int(sys.arg[3])+1
         work_min = int(sys.argv[1])
         rest_min = int(sys.argv[2])
     except IndexError:
-        print("python3 pomodoro.py [number of cycles] [work duration] [rest duration]")
+        print("python3 pomodoro.py [work duration] [rest duration]")
     else:
-        for i in range(iterations):
-            # start working
-            os.system(say "Start working")
-            time.sleep(work_min*60)
-            # stop working and start rest
-            os.system(say "Break time")
-            time.sleep(rest_min*60)
-        os.system(say "Study time over")
+        # start working
+        os.system('say "Start working"')
+        time.sleep(work_min*60)
+        # stop working and start rest
+        os.system('say "Break time"')
+        time.sleep(rest_min*60)
+        os.system('say "Study session over"')
 
 
 if __name__ == "__main__":
